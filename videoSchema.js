5f5e3fcef35337dad31a12999bb6bbbb637cdc7e
David Isbitski <disbitski@outlook.com>
2018-07-03 17:33:47 -0400

    Add files via upload

---
+++ videoSchema.js
@@ -0,0 +1,30 @@
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "my mobile video",
+            "intents": [
+                {
+                    "name": "AMAZON.CancelIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": []
+                },
+                {
+                    "name": "MainIntent",
+                    "slots": [],
+                    "samples": [
+                        "hello",
+                        "show me"
+                    ]
+                }
+            ],
+            "types": []
+        }
+    }
+}
